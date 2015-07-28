be662c10e9bee62be4955341d2ff0d4fba260af1
douglasbolis <douglas.lima@xdevel.com.br>
2015-07-28 18:03:38 -0300

    Implementado código da sequência de fibonacci.

---
+++ c/Problema2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void) {
+
+    int qtdElements = 20;
+    int elemA = 1;
+    int elemB = 1;
+    int auxC = 0;
+    int i;
+
+    printf("%d %d ", elemA, elemB);
+
+    for (i = 2; i < qtdElements; i++) {
+        auxC = elemA;
+        elemA = elemA + elemB;
+        elemB = auxC;
+
+        printf("%d ", elemA);
+    }
+}
